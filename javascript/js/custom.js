dd66dd22ac658112f0476eeff89d3821d3884e2e
samjhana05 <amazingsamjhana05@gmail.com>
2021-02-09 12:48:38 +0545

    update

--- javascript/js/custom.js
+++ javascript/js/custom.js
@@ -1,1 +1,21 @@
-alert("test")
+//***pop up***
+
+// alert("test")
+// window.alert('test');
+
+// document.write("<h1>Hello JavaScript<\h1>");
+
+// document.getElementById('test').innerHTML="Hello World";
+
+// document.getElementById('test').style.color='red';
+
+// setTimeout(function (){
+//     alert("test");
+// },2000)
+
+// setInterval(function (){
+//     alert("test");
+// },3000)
+
+// confirm('Are you sure?')
+// prompt('Enter your name')
